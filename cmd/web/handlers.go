d2b39651e140494968f50c97ceabc0dbd2740068
jackmford <jackmitchellfordyce@gmail.com>
2024-05-02 09:32:03 -0500

    feat: refactoring

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"flag"
 	"html/template"
-	"log"
 	"net/http"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-	"vimtricks.jackmitchellfordyce.com/internal/models"
 )
-
-type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	tricks   *models.TrickModel
-}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -69,50 +58,4 @@ func (app *application) trickCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	app.infoLog.Print(id)
-}
-
-func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
-
-	dsn := flag.String("dsn", "web:pass@/vimtricks?parseTime=true", "MySQL data source name")
-
-	flag.Parse()
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
-
-	db, err := openDB(*dsn)
-	if err != nil {
-		errorLog.Fatal(err)
-	}
-
-	defer db.Close()
-
-	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		tricks:   &models.TrickModel{DB: db},
-	}
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux := http.NewServeMux()
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/create", app.trickCreate)
-
-	infoLog.Print("Starting server on :4000")
-	err = http.ListenAndServe(*addr, mux)
-	errorLog.Fatal(err)
-}
-
-func openDB(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	if err = db.Ping(); err != nil {
-		return nil, err
-	}
-	return db, nil
 }
